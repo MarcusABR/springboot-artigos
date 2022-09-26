0895ba982dde52b4470125d63e4fabbe0d2d74c4
Marcus Abrantes <marcusabrantes1337@gmail.com>
2022-09-26 09:06:48 -0300

    First commit

---
+++ src/test/java/br/com/bb/letscode/artigos/ArtigosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bb.letscode.artigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtigosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
