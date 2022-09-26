a2593e848cccef12bbec1bd22a0e516925de548c
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-26 18:10:33 -0300

    Update Usuario entity

--- src/main/java/br/com/bb/letscode/artigos/entity/Usuario.java
+++ src/main/java/br/com/bb/letscode/artigos/entity/Usuario.java
@@ -4,13 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
+@Entity
 public class Usuario {
+    @Id
+    private Long id;
     private String nome;
-    private String id;
-    private String endereco;
+    private String email;
+    private String senha;
+    @OneToOne
+    private Endereco endereco;
 }
