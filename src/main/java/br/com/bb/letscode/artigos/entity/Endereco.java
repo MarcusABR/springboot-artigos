a8f195278fdc73f47320efa1192e0089bd46d5b2
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-26 18:11:07 -0300

    Add: Endereco.java

---
+++ src/main/java/br/com/bb/letscode/artigos/entity/Endereco.java
@@ -0,0 +1,30 @@
+package br.com.bb.letscode.artigos.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "uf", length = 2)
+    private String uf;
+    private String cidade;
+    @Column(name = "cep", length = 9)
+    private String cep;
+    private String bairro;
+    private String logradouro;
+
+    @OneToOne(mappedBy = "endereco")
+    private Usuario usuario;
+}
