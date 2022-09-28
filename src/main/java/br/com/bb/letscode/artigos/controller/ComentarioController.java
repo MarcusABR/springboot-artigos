bf87e66134b251b5fc01071f37a20002bf9a634e
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-28 18:53:08 -0300

    Update ComentarioController.java

--- src/main/java/br/com/bb/letscode/artigos/controller/ComentarioController.java
+++ src/main/java/br/com/bb/letscode/artigos/controller/ComentarioController.java
@@ -1,32 +1,65 @@
 package br.com.bb.letscode.artigos.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import br.com.bb.letscode.artigos.entity.Comentario;
+import br.com.bb.letscode.artigos.entity.Usuario;
+import br.com.bb.letscode.artigos.service.ComentarioService;
+
 public class ComentarioController {
     
     @Autowired
-    final UsuarioRepository usuarioRepository;
+    final ComentarioService comentarioService;
 
-    public UsuarioController(UsuarioRepository usuarioRepository) {
-        this.usuarioRepository = usuarioRepository;
+    public ComentarioController(ComentarioService comentarioService) {
+        this.comentarioService = comentarioService;
     }
 
     @PostMapping
-    public ResponseEntity<Usuario> insert(@RequestBody Usuario usuario){
-        log.info(usuario.toString());
-        return ResponseEntity.ok(usuarioRepository.save(usuario));
+    public ResponseEntity<Comentario> insert(@RequestBody Comentario comentario){
+        return ResponseEntity.ok(comentarioService.save(comentario));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getById(@PathVariable("id") Long id){
-        Usuario usuario = new Usuario();
-        usuario.setId(id);
-        return ResponseEntity.ok(usuario);
+    public ResponseEntity<Usuario> getByUserId(@PathVariable("id") Long userd){
+        return null;
     }
 
-    @GetMapping(value = {
-        "/",
-        "/{id}"
-    })
-    public ResponseEntity<List<Usuario>> getAll(@PathVariable(value = "id", required = false) Long id){
-        return ResponseEntity.ok(usuarioRepository.findAll());
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getByArticle(@PathVariable("id") Long articleId, @PathVariable(value = "user", required = false) Long id){
+        return null;
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Comentario>> getAll(@PathVariable(value = "id", required = false) Long id){
+        return ResponseEntity.ok(comentarioService.findAll());
+    }
+
+    @DeleteMapping
+    public void delete(@PathVariable("id") Long id){
+        comentarioService.delete(id);
+    }
+
+    // @GetMapping(value = {
+    //     "/",
+    //     "/{id}"
+    // })
+    // public ResponseEntity<List<Usuario>> getAll(@PathVariable(value = "id", required = false) Long id){
+    //     return ResponseEntity.ok(usuarioRepository.findAll());
+    // }
+
+
+    // List<Comentario> findByArticle(Long articleId);
+
+    // List<Comentario> findByArticle(Long articleId, Long usuarioId);
+
+    // List<Comentario> findByUserId(Long articleId);
 }
