929c40dc6a30a842d7a33d3c3a3f3444ea1baf5f
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-28 18:25:35 -0300

    Update Usuario.java

--- src/main/java/br/com/bb/letscode/artigos/entity/Usuario.java
+++ src/main/java/br/com/bb/letscode/artigos/entity/Usuario.java
@@ -5,8 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
 
@@ -22,6 +25,7 @@ public class Usuario {
     private String nome;
     private String email;
     private String senha;
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "en0dereco_id", referencedColumnName = "id")
     private Endereco endereco;
 }
