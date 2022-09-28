43609ef68e827a7331168b6c7d03a077bbcf73c5
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-28 18:31:01 -0300

    Update ComentarioServiceImpl.java

--- src/main/java/br/com/bb/letscode/artigos/service/impl/ComentarioServiceImpl.java
+++ src/main/java/br/com/bb/letscode/artigos/service/impl/ComentarioServiceImpl.java
@@ -1,5 +1,54 @@
 package br.com.bb.letscode.artigos.service.impl;
+import br.com.bb.letscode.artigos.service.ComentarioService;
 
-public class ComentarioServiceImpl {
-    
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.bb.letscode.artigos.entity.Artigo;
+import br.com.bb.letscode.artigos.entity.Comentario;
+import br.com.bb.letscode.artigos.repository.ComentarioRepository;
+
+
+@Service
+public class ComentarioServiceImpl implements ComentarioService{
+    final ComentarioRepository comentarioRepository;
+
+    public ComentarioServiceImpl(ComentarioRepository comentarioRepository){
+        this.comentarioRepository = comentarioRepository;
+    }
+
+   
+
+    @Override
+    public void delete(Long id) {
+        comentarioRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Comentario> findAll() {
+        return comentarioRepository.findAll();
+    }
+
+    @Override
+    public List<Comentario> findByArticle(Long articleId) {
+        return null;
+    }
+
+    @Override
+    public List<Comentario> findByArticle(Long articleId, Long usuarioId) {
+        return null;
+    }
+
+    @Override
+    public List<Comentario> findByUserId(Long articleId) {
+        return null;
+    }
+
+
+
+    @Override
+    public Comentario save(Comentario comentario) {
+        return comentarioRepository.save(comentario);
+    }
 }
