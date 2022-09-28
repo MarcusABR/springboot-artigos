3e67103cb11c603a49cf7b6b8f8717e3b3c814ff
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-28 18:25:18 -0300

    Update Artigo.java

--- src/main/java/br/com/bb/letscode/artigos/entity/Artigo.java
+++ src/main/java/br/com/bb/letscode/artigos/entity/Artigo.java
@@ -1,13 +1,20 @@
 package br.com.bb.letscode.artigos.entity;
 
 import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity
 public class Artigo{
+    @Id
+    private long id;
     private String codigoArtigo;
     private String codigoUsuario;
     private String titulo;
