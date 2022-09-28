1ab53eab750e43414d30d70f2e46d6eb71993daf
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-28 18:24:32 -0300

    Update Comentario.java

--- src/main/java/br/com/bb/letscode/artigos/entity/Comentario.java
+++ src/main/java/br/com/bb/letscode/artigos/entity/Comentario.java
@@ -1,5 +1,11 @@
 package br.com.bb.letscode.artigos.entity;
 import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
 import br.com.bb.letscode.artigos.entity.enums.TipoComentario;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,9 +13,12 @@ import lombok.Getter;
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity
 public class Comentario {
+    @Id
     private Long id;
     private String nomeUsuario;
     private String codigoArtigo;
+    @Enumerated(EnumType.STRING)
     private TipoComentario tipo;
 }
