686ba35cc3a777a96a23ff9c40742918d607e7bc
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-28 18:26:34 -0300

    Update ArtigoService.java

--- src/main/java/br/com/bb/letscode/artigos/service/ArtigoService.java
+++ src/main/java/br/com/bb/letscode/artigos/service/ArtigoService.java
@@ -1,11 +1,22 @@
 package br.com.bb.letscode.artigos.service;
 
-public class ArtigoService {
-    //GetById
-    //GetAllByUser
-    //CreateArticle
-    //DeleteArticle
-    //GetAll
-    //UpvoteArticle
-    //DownvoteArticle
+import java.util.List;
+
+import br.com.bb.letscode.artigos.entity.Artigo;
+
+public interface ArtigoService {
+
+    Artigo save(Artigo artigo);
+
+    void delete(Long id);
+
+    public Artigo getById(Long id);
+
+    List<Artigo> findAll();
+
+    List<Artigo> findByUserId(Long articleId);
+
+    void upvoteArticle(Long id);
+
+    void dowvoteArticle(Long id);
 }
