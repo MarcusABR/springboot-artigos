4fff9c9ab2c134ede58fa111ddf484921ab7b25c
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-28 18:30:41 -0300

    Update CampoServiceImpl.java

--- src/main/java/br/com/bb/letscode/artigos/service/impl/CampoServiceImpl.java
+++ src/main/java/br/com/bb/letscode/artigos/service/impl/CampoServiceImpl.java
@@ -1,5 +1,42 @@
 package br.com.bb.letscode.artigos.service.impl;
+import br.com.bb.letscode.artigos.service.CampoService;
 
-public class CampoServiceImpl {
-    
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.bb.letscode.artigos.entity.Campo;
+import br.com.bb.letscode.artigos.repository.CampoRepository;
+
+@Service
+public class CampoServiceImpl implements CampoService{
+    final CampoRepository campoRepository;
+
+    public CampoServiceImpl(CampoRepository campoRepository){
+        this.campoRepository = campoRepository;
+    }
+
+    @Override
+    public Campo save(Campo campo) {
+       
+        return  campoRepository.save(campo);
+    }
+
+    @Override
+    public List<Campo> findAll() {
+        return campoRepository.findAll();
+    }
+
+    @Override
+    public Campo getById(String nome) {
+        // TODO Auto-generated method stub
+        return  campoRepository.findById(nome).get();
+    }
+
+    @Override
+    public void delete(String nome) {
+        campoRepository.deleteById(nome);        
+    }
+
+
 }
