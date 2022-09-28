cc3dabb4d7c11ab5d0f9d58a8c171b3bbac4cc07
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-28 18:31:18 -0300

    Update UsuarioServiceImpl.java

--- src/main/java/br/com/bb/letscode/artigos/service/impl/UsuarioServiceImpl.java
+++ src/main/java/br/com/bb/letscode/artigos/service/impl/UsuarioServiceImpl.java
@@ -1,5 +1,33 @@
 package br.com.bb.letscode.artigos.service.impl;
+import br.com.bb.letscode.artigos.service.UsuarioService;
 
-public class UsuarioServiceImpl {
-    
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.bb.letscode.artigos.entity.Usuario;
+import br.com.bb.letscode.artigos.repository.UsuarioRepository;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService{
+    final UsuarioRepository usuarioRepository;
+
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public Usuario save(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
+    }
+
+    @Override
+    public Usuario getById(Long userId) {
+        return null;
+    }
 }
