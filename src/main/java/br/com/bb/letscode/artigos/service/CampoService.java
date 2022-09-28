ddc4c93fcfbe1abd60f27e057b0209767d7ecca4
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-28 18:26:56 -0300

    Update CampoService.java

--- src/main/java/br/com/bb/letscode/artigos/service/CampoService.java
+++ src/main/java/br/com/bb/letscode/artigos/service/CampoService.java
@@ -1,6 +1,15 @@
 package br.com.bb.letscode.artigos.service;
 
-public class CampoService {
-    //CRUD
-    //GetAll
+import java.util.List;
+
+import br.com.bb.letscode.artigos.entity.Campo;
+
+public interface CampoService {
+    Campo save(Campo campo);
+
+    List<Campo> findAll();
+
+    Campo getById(String nome);
+
+    void delete(String nome);
 }
