2a6dabb4fbd39ba8a8364597a14b911f58af04eb
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-29 16:20:05 -0300

    Update Comentario.java

--- src/main/java/br/com/bb/letscode/artigos/entity/Comentario.java
+++ src/main/java/br/com/bb/letscode/artigos/entity/Comentario.java
@@ -6,18 +6,23 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 
 import br.com.bb.letscode.artigos.entity.enums.TipoComentario;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Comentario {
     @Id
     private Long id;
+    @NotNull(message = "É necessário escrever algo no comentário para publicá-lo!")
+    private String texto;
     private String nomeUsuario;
     private String codigoArtigo;
     @Enumerated(EnumType.STRING)
