7df738e74f64e4bb95427dbee788db3609e86901
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-29 16:19:33 -0300

    Update Artigo.java

--- src/main/java/br/com/bb/letscode/artigos/entity/Artigo.java
+++ src/main/java/br/com/bb/letscode/artigos/entity/Artigo.java
@@ -7,24 +7,32 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Artigo{
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotNull(message = "É necessário um título")
     private String titulo;
+    @NotNull(message = "É necessário um texti")
     private String texto;
     private String lingua;
     private Integer upvote;
