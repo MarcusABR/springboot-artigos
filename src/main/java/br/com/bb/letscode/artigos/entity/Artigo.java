c499d20218fd2b069284f71a03ee0f40db37a5a1
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-29 11:53:38 -0300

    Update Artigo.java

--- src/main/java/br/com/bb/letscode/artigos/entity/Artigo.java
+++ src/main/java/br/com/bb/letscode/artigos/entity/Artigo.java
@@ -2,8 +2,17 @@ package br.com.bb.letscode.artigos.entity;
 
 import lombok.Setter;
 
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,11 +24,27 @@ import lombok.Getter;
 public class Artigo{
     @Id
     private long id;
-    private String codigoArtigo;
-    private String codigoUsuario;
     private String titulo;
     private String texto;
     private String lingua;
     private Integer upvote;
     private Integer downvote;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "artigo")
+    // @JoinColumn(name="comentario_id")
+    private List<Comentario> comentarios;
+
+    @ManyToOne
+    private Usuario usuario;
+    
+    @ManyToMany
+    @JoinTable(
+        name = "artigo_campos",
+        // joinColumns = @JoinColumn(name = "campo_nome", referencedColumnName = ""),
+        joinColumns = @JoinColumn(name = "campo_nome"),
+        // inverseJoinColumns = @JoinColumn(name = "artigo_id", referencedColumnName = "id")
+        inverseJoinColumns = @JoinColumn(name = "artigo_id")
+    )
+
+    private Set<Campo> campos;
 }
