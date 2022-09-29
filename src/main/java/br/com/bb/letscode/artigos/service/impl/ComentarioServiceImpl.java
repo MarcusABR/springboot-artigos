76b022483a10a3656c1344fccf60513a89c8991a
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-29 16:24:11 -0300

    Update ComentarioServiceImpl.java

--- src/main/java/br/com/bb/letscode/artigos/service/impl/ComentarioServiceImpl.java
+++ src/main/java/br/com/bb/letscode/artigos/service/impl/ComentarioServiceImpl.java
@@ -3,6 +3,10 @@ import br.com.bb.letscode.artigos.service.ComentarioService;
 
 import java.util.List;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.bb.letscode.artigos.entity.Artigo;
@@ -26,13 +30,26 @@ public class ComentarioServiceImpl implements ComentarioService{
     }
 
     @Override
-    public List<Comentario> findAll() {
-        return comentarioRepository.findAll();
+    public Page<Comentario> findAll(String filter, Pageable pageable) {
+        return comentarioRepository.findByNomeContainingAndTextoContaining(filter, filter, pageable);
     }
 
     @Override
     public List<Comentario> findByArticle(Long articleId) {
-        return null;
+        Artigo artigo = new Artigo();
+        artigo.setId(articleId);
+        Comentario comentario = new Comentario();
+        comentario.setArtigo(artigo);
+
+        final ExampleMatcher exampleMatcher = 
+                ExampleMatcher  
+                        .matchingAny();
+
+        Example<Comentario> comentarioExample = Example.of(comentario, exampleMatcher);
+
+        
+
+        return comentarioRepository.findAll(comentarioExample);
     }
 
     @Override
