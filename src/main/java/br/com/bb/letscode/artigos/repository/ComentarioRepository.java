405d9aec06f25cd0c853a834aa8a072948cdeafc
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-29 16:21:14 -0300

    Update ComentarioRepository.java

--- src/main/java/br/com/bb/letscode/artigos/repository/ComentarioRepository.java
+++ src/main/java/br/com/bb/letscode/artigos/repository/ComentarioRepository.java
@@ -1,9 +1,10 @@
 package br.com.bb.letscode.artigos.repository;
 import br.com.bb.letscode.artigos.entity.Comentario;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ComentarioRepository extends JpaRepository<Comentario, Long>{
-    
+    public Page<Comentario> findByNomeContainingAndTextoContaining(String nome, String texto, Pageable pageable);
 }
-
-
