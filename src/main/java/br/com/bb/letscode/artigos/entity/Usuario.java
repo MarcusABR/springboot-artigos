ff33ac022ebb4eaa10a925e999c8209866b6f177
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-29 16:20:18 -0300

    Update Usuario.java

--- src/main/java/br/com/bb/letscode/artigos/entity/Usuario.java
+++ src/main/java/br/com/bb/letscode/artigos/entity/Usuario.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
 
 
 @Getter
@@ -25,8 +26,11 @@ import javax.persistence.OneToOne;
 public class Usuario {
     @Id
     private Long id;
+    @NotNull(message = "Insira um nome.")
     private String nome;
+    @NotNull(message = "Insira um email.")
     private String email;
+    @NotNull(message = "Insira uma senha.")
     private String senha;
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "endereco_id", referencedColumnName = "id")
