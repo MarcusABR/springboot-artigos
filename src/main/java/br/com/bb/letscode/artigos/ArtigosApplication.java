71e9aae877ad3391fe6711ce2c20a9b465a6316e
MarcusABR <marcusabrantes1337@gmail.com>
2022-09-29 16:23:15 -0300

    Update ArtigosApplication.java

--- src/main/java/br/com/bb/letscode/artigos/ArtigosApplication.java
+++ src/main/java/br/com/bb/letscode/artigos/ArtigosApplication.java
@@ -2,8 +2,10 @@ package br.com.bb.letscode.artigos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ArtigosApplication {
 
 	public static void main(String[] args) {
