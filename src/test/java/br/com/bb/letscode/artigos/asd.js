fc6b8328ddf4024190a0109d9b269f005b4dbf5b
MarcusABR <marcusabrantes1337@gmail.com>
2022-11-04 14:16:17 -0300

    Update asd.js

--- src/test/java/br/com/bb/letscode/artigos/asd.js
+++ src/test/java/br/com/bb/letscode/artigos/asd.js
@@ -1,114 +1,64 @@
 export default class Graph {
-  constructor() {
-    this.edgeList = new Map();
-    this.nodeList = new Map();
-    this.visited = [];
-    this.height = 0;
-  }
-
-  create(data, idName, listName){
-    this.convertData(data, idName, listName);
-    this.update(idName);
-    console.log(this.nodeList)
-  }
-
-  convertData(data, idName, listName) {
-    const id = data[idName];
-    
-    if (!this.nodeList.get(id)) {
-      const {[listName]: remove, ...obj} = data
-      this.nodeList.set(id, obj)
-      this.edgeList.set(id, []);
+    constructor() {
+      this.edgeList = new Map();
+      this.nodeList = new Map();
+      this.nodeByHeight = [];
+      this.visited = [];
+      this.height = 0;
     }
 
-    for (let child of data[listName]) {
-      this.edgeList.get(id).push(child[idName]);
-      this.convertData(child, idName, listName);
+    get getEdgeList(){
+        return this.edgeList;
     }
-  }
 
-  depthFirstSearch(key, height, idName){
-    if(height > this.height ) this.height = height;
-    if(this.visited[key]) return;
-
-    this.visited.push(key);
-    this.nodeList.get(key).level = height;
-
-    for( let v of this.edgeList.get(key)){ 
-      this.depthFirstSearch(v, height+1, idName)
+    get getNodeList(){
+        return this.nodeList;
     }
+
+    get getNodeByHeight() {
+        return this.nodeByHeight
+    }
+  
+    create(data, idName, listName){
+      this.convertData(data, idName, listName);
+      this.update(idName);
+    }
+  
+    convertData(data, idName, listName) {
+      const id = data[idName];
+  
+      if (!this.nodeList.get(id)) {
+        const {[listName]: remove, ...obj} = data
+        this.nodeList.set(id, obj)
+        this.edgeList.set(id, []);
+      }
+  
+      for (let child of data[listName]) {
+        this.edgeList.get(id).push(child[idName]);
+        this.convertData(child, idName, listName);
+      }
+    }
+  
+    depthFirstSearch(key, height, idName){
+      if(height > this.height ) this.height = height;
+      if(this.visited[key]) return;
+  
+      this.visited.push(key);
+      if(!this.nodeByHeight[height-1] ) this.nodeByHeight[height-1] = []
+      this.nodeByHeight[height-1].push(this.nodeList.get(key))
+  
+      for( let v of this.edgeList.get(key)){ 
+        this.depthFirstSearch(v, height+1, idName)
+      }
+    }
+
+    update(idName){
+      this.visited = [];
+      this.height = 0
+      const [ firstKey ] = this.edgeList.keys();
+      this.depthFirstSearch(firstKey, 1, idName)
+    }
+  
+  
   }
-
-  update(idName){
-    this.visited = [];
-    this.height = 0
-    const [ firstKey ] = this.edgeList.keys();
-    this.depthFirstSearch(firstKey, 1, idName)
-  }
-
-
-}
-
-
-import React, { useState, useCallback, useEffect } from "react";
-import { scaleBand, scaleLinear, max } from "d3";
-import Graph from "./utils/Graph";
-
-const data = {
-  name: "node 1",
-  children: [
-    {
-      name: "node 2",
-      children: [
-        {
-          name: "node 4",
-          children: [],
-        },
-      ],
-    },
-    {
-      name: "node 3",
-      children: [
-        {
-          name: "node 5",
-          children: [
-            {
-              name: "node 6",
-              children: [],
-            },
-          ],
-        },
-      ],
-    },
-  ],
-};
-const width = 960;
-const height = 500;
-
-const App = () => {
-  const graph = new Graph();
-  graph.create(data, "name", "children");
-
-  // const xScale = scaleBand()
-  //   .domain(data.map((d) => d.name))
-  //   .range([0, height]);
-
-  // const yScale = scaleBand()
-  //   .domain(data.map((d) => d.name))
-  //   .range([0, height]);
-
-  return (
-    <svg width={width} height={height}>
-      {/* {data.map((d) => (
-        <rect
-          key={d.Country}
-          y={yScale(d.Country)}
-          width={xScale(d.Population)}
-          height={yScale.bandwidth()}
-        />
-      ))} */}
-    </svg>
-  );
-};
-
-export default App;
+  
